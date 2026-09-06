03852ebe17195ffeb345b46d2118ea29895b5771
Sweta Jha <swetajhaofficial11@gmail.com>
2026-09-06 11:54:40 +0530

    Time: 0 ms (100%), Space: 8.4 MB (99.56%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool check(int n,int mid){
+        if(mid<=n/mid)return true;
+        return false;
+    }
+    int mySqrt(int x) {
+        if(x==0)return 0;
+        int low=1, high=x;
+        int ans=0;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(check(x,mid)){
+               ans= mid;
+               low=mid+1;
+            }
+            else high=mid-1;
+        }
+        return ans;
+    }
+};
