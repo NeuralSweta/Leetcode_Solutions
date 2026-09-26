77814caa9b254b173a2324f34720a12d8f8c9dcb
Sweta Jha <swetajhaofficial11@gmail.com>
2026-09-26 09:39:22 +0530

    Time: 0 ms (100%), Space: 15.7 MB (75.03%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.cpp
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
-    int helper(vector<int>&nums,int i, vector<int>&earn, vector<int>&dp){
-        int n= earn.size();
-        if(i>=n)return 0;
-        if(dp[i]!=-1)return dp[i];
-        int pick=earn[i]+helper(nums,i+2,earn,dp);
-        int skip= helper(nums,i+1,earn,dp);
-        return dp[i]=max(pick,skip);
-    }
     int deleteAndEarn(vector<int>& nums) {
         int maxi=*max_element(nums.begin(),nums.end());
         vector<int>earn(maxi+1); // store earn of a particular num (2,3,4)= (4,9,4)// then apply house robber on earn
         for(int ele:nums)earn[ele]+=ele;
         vector<int>dp(maxi+1,-1);
-        return helper(nums,0,earn,dp);   
+          dp[0]=earn[0];
+          dp[1]=max(earn[0],earn[1]);
+         for(int i=2;i<=maxi;i++){
+            dp[i]=max(earn[i]+dp[i-2],dp[i-1]);
+         }
+      return dp[maxi];   
     }
 };
